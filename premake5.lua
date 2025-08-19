1b72ba2e0c336f7443f6d9f44e885d5275efa91b
Siddhant <shindesiddhant709@gmail.com>
2025-08-19 23:03:03 +0530

    Fixed compilation errors

--- premake5.lua
+++ premake5.lua
@@ -1,71 +1,32 @@
 workspace "NVRHI_Renderer"
-	architecture "x64"
-	startproject "NVRHI_Renderer"
+    architecture "x64"
+    startproject "NVRHI_Renderer"
 
-	configurations
-	{
-		"Debug",
-		"Release",
-		"Dist"
-	}
+    configurations { "Debug", "Release", "Dist" }
+    outputdir = "%{cfg.buildcfg}-%{cfg.system}-%{cfg.architecture}"
 
-	outputdir = "%{cfg.buildcfg}-%{cfg.system}-%{cfg.architecture}"
+    IncludeDir = {}
+	IncludeDir["nvrhi_vulkan_thridparty"] = "vendor/nvrhi/thirdparty/Vulkan-Headers/include"
+	IncludeDir["nvrhi_dx_thridparty"] = "vendor/nvrhi/thirdparty/DirectX-Headers/include"
 
-	IncludeDir = {}
-	IncludeDir["nvrhi"] = "vendor/nvrhi/include"
-	IncludeDir["nvrhi_thridparty"] = "vendor/nvrhi/thirdparty/Vulkan-Headers/include"
-	
-project "NVRHI_Renderer"
-	kind "StaticLib"
-	language "C++"
-	cppdialect "C++17"
-	staticruntime "on"
-
-	targetdir ("bin/" .. outputdir .. "/%{prj.name}")
-	objdir ("bin-int/" .. outputdir .. "/%{prj.name}")
-
-	files
-	{
-		"src/**.h",
-		"src/**.cpp",
-		"vendor/nvrhi/src/vulkan/**.h",
-		"vendor/nvrhi/src/vulkan/**.cpp"
-	}
-	
-	includedirs
-	{
-		"%{IncludeDir.nvrhi}",
-		"%{IncludeDir.nvrhi_thridparty}"
-	}
-
-	links
-	{
-		"vendor/nvrhi/out/build/x64-Debug/nvrhi_vk.lib",
-		"vendor/nvrhi/out/build/x64-Debug/nvrhi.lib"
-	}
-
-	filter "system:windows"
-		systemversion "latest"
-
-	defines
-	{
-		"GLFW_INCLUDE_NONE"
-	}
-
-	filter "configurations:Debug"
-		defines "DEBUG"
-		symbols "on"
-		buildoptions "/utf-8"
-		runtime "Debug"
-
-	filter "configurations:Release"
-		defines "RELEASE"
-		optimize "on"
-		buildoptions "/utf-8"
-		runtime "Release"
-
-	filter "configurations:Dist"
-		defines "DIST"
-		optimize "on"
-		buildoptions "/utf-8"
-		runtime "Release"
+    project "NVRHI_Renderer"
+        kind "ConsoleApp"
+        language "C++"
+        cppdialect "C++17"
+        
+        targetdir ("bin/" .. outputdir .. "/%{prj.name}")
+        objdir ("bin-int/" .. outputdir .. "/%{prj.name}")
+        
+        includedirs {
+            "vendor/nvrhi/include",
+		    "%{IncludeDir.nvrhi_vulkan_thridparty}",
+		    "%{IncludeDir.nvrhi_dx_thridparty}"
+        }
+        
+        files {
+            "src/**.h",
+            "src/**.cpp"
+        }
+        
+        filter "system:windows"
+            systemversion "latest"
